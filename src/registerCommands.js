b632c09a8698a633c54cb45b01d60fdb1288eaf6
Kaydan Kotze <kaydankotze17@gmail.com>
2026-09-16 16:56:49 +0200

    Refresh Discord slash commands

--- src/registerCommands.js
+++ src/registerCommands.js
@@ -3,10 +3,18 @@ import { REST, Routes } from "discord.js";
 import { commands } from "./commands.js";
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+const clientId = process.env.DISCORD_CLIENT_ID;
 
 await rest.put(
-  Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
+  Routes.applicationCommands(clientId),
+  { body: [] }
+);
+
+console.log("Cleared all global Discord commands.");
+
+await rest.put(
+  Routes.applicationCommands(clientId),
   { body: commands }
 );
 
-console.log(`Registered ${commands.length} global commands.`);
+console.log(`Registered ${commands.length} current commands.`);
